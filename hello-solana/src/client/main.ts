2accce5f48e65c0c2a3754f8a78660bc0205d784
Kirill_Aksenov <aksenovki@gmail.com>
2024-09-21 12:27:09 +1000

    Run transaction

--- hello-solana/src/client/main.ts
+++ hello-solana/src/client/main.ts
@@ -1,83 +1,68 @@
 import {
-  Keypair,
   Connection,
-  PublicKey,
-  LAMPORTS_PER_SOL,
   TransactionInstruction,
   Transaction,
   sendAndConfirmTransaction,
+  PublicKey,
 } from "@solana/web3.js";
-import fs from "mz/fs";
+import { fs } from "mz";
 import path from "path";
 
-/*
-  Our keypair we used to create the on-chain Rust program
-*/
-const PROGRAM_KEYPAIR_PATH = path.join(
-  path.resolve(__dirname, "../../dist/program"),
-  "hello_solana-keypair.json"
-);
+import os from "os";
+
+const KEY_PATH = path.join(os.homedir(), ".config", "solana", "id.json");
 
 async function main() {
   console.log("Launch...");
-  /*
-  Connect to Solana DEV net
-  */
-  let connection = new Connection("https://api.devnet.solana.com", "confirmed");
 
-  /*
-  Get our program's public key
-  */
-
-  const secretKeyString = await fs.readFile(PROGRAM_KEYPAIR_PATH, {
+  const connection = new Connection(
+    "https://api.devnet.solana.com",
+    "confirmed"
+  );
+  const secretKeyString = await fs.readFile(KEY_PATH, {
     encoding: "utf8",
   });
 
-  const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
-  const programKeypair = Keypair.fromSecretKey(secretKey);
-  let programId: PublicKey = programKeypair.publicKey;
-
-  /*
-  Generate an account (keypair) to transact with our program
-  */
-
-  const triggerKeypair = Keypair.generate();
-  const airdropRequest = await connection.requestAirdrop(
-    triggerKeypair.publicKey,
-    LAMPORTS_PER_SOL
+  const programId = new PublicKey(
+    "AurSBVWpYqQmpTbq2givGMUEWStNwaksMu1P3ssT1npR"
   );
 
-  // Confirm the transaction using the new method
-  const latestBlockhash = await connection.getLatestBlockhash();
-
-  await connection.confirmTransaction({
-    signature: airdropRequest,
-    blockhash: latestBlockhash.blockhash,
-    lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
-  });
-
-  /*
-  Conduct a transaction with our program
-  */
-  console.log("--Pinging Program ", programId.toBase58());
   const instruction = new TransactionInstruction({
     keys: [
-      { pubkey: triggerKeypair.publicKey, isSigner: false, isWritable: true },
+      {
+        pubkey: new PublicKey("3xsY1pZgQyZvQmzuybcqb93MiUGeRpurSVurwr1x9o2y"),
+        isSigner: false,
+        isWritable: true,
+      },
     ],
     programId,
     data: Buffer.alloc(0),
   });
-  await sendAndConfirmTransaction(
-    connection,
-    new Transaction().add(instruction),
-    [triggerKeypair]
-  );
+
+  try {
+    await sendAndConfirmTransaction(
+      connection,
+      new Transaction().add(instruction),
+      [
+        {
+          publicKey: new PublicKey(
+            "3xsY1pZgQyZvQmzuybcqb93MiUGeRpurSVurwr1x9o2y"
+          ),
+          secretKey: Uint8Array.from(JSON.parse(secretKeyString)),
+        },
+      ]
+    );
+    console.log("Transaction successful!");
+  } catch (err) {
+    console.error("SendTransactionError caught!");
+    console.error("Transaction failed with message:", err.message);
+  }
 }
 
 main().then(
   () => process.exit(),
   (err) => {
-    console.error(err);
+    console.error("Error in main:", err);
     process.exit(-1);
   }
 );
